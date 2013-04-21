96031c0b9df93e9339d6563a3c6cb66897cf50d5
Chris Robber <rauberc@t-online.de>
2013-04-21 18:51:00 +0200

    erster Test mit wakanda

---
+++ wakrechner/WebFolder/index.waPage/scripts/index.js
@@ -0,0 +1,27 @@
+﻿
+WAF.onAfterInit = function onAfterInit() {// @lock
+
+// @region namespaceDeclaration// @startlock
+	var btMultipliziere = {};	// @button
+	var btAddiere = {};	// @button
+// @endregion// @endlock
+
+// eventHandlers// @lock
+
+	btMultipliziere.click = function btMultipliziere_click (event)// @startlock
+	{// @endlock
+		txtErgebnis = parseInt(txtZahl1) * parseInt(txtZahl2);
+		sources.txtErgebnis.sync();
+	};// @lock
+
+	btAddiere.click = function btAddiere_click (event)// @startlock
+	{// @endlock
+		txtErgebnis = parseInt(txtZahl1) + parseInt(txtZahl2);
+		sources.txtErgebnis.sync();	
+	};// @lock
+
+// @region eventManager// @startlock
+	WAF.addListener("btMultipliziere", "click", btMultipliziere.click, "WAF");
+	WAF.addListener("btAddiere", "click", btAddiere.click, "WAF");
+// @endregion
+};// @endlock
